f5d6b96f3f74375ac24569caceee5e0d4f6fb406
rishab raj verma <rishabrajverma44@gmail.com>
2025-07-23 23:19:59 +0530

    feat : add form html block

--- src/components/App.ts
+++ src/components/App.ts
@@ -1,9 +1,10 @@
+import { Form } from "./Form";
+
 export function renderApp() {
   const root = document.getElementById("app");
   const app = document.createElement("div");
   app.className = "my-app";
-  app.innerHTML = "<h1>app file</h1>";
   //insert elements in seperate div with class name my-app
-
+  app.appendChild(Form());
   root?.append(app);
 }
